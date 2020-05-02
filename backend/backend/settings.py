00b28ad6b351d2bab927d9611328853261b4b541
vvxhid <wahidmadano@gmail.gom>
2020-05-02 06:42:01 +0100

    delete migrations and change DB settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,22 +87,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #https://docs.djangoproject.com/en/3.0/ref/settings/
 #databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'madina-tic',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'postgres',
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'madina-tic',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
